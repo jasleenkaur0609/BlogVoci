3d046be50236b34daf9de33433be1d794b509422
Jasleen Kaur <jasleensejal2003@gmail.com>
2025-05-17 22:07:18 +0530

    Made changes in route.js

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,6 +1,19 @@
+import { ConnectDB } from "@/lib/config/db";
+
 const { NextResponse } = require("next/server");
+
+const LoadDB =async()=>{
+    await ConnectDB();
+}
+
+LoadDB();
+
 
 export async function GET(request) {
     console.log("Blog GET Hit");
     return NextResponse.json({msg:"Api working"});
+}
+
+export async function POST(request){
+    const formData = await request.formData();
 }
